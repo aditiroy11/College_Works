ff7034cbb57d367ac995151866f7fd3af29965ee
aditiroy11 <aroy34535@gmail.com>
2022-08-26 19:21:21 +0530

    3rd commit

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{ 
+    int l,area;
+    printf("enter the length=");
+    scanf("%d",&l);
+    area=l*l;
+    printf("the area of square will be=%d\n",area);
+    return 0;
+}
