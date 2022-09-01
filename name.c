db2b264dbd1284b6a69404a500baf997d1835b73
aditiroy11 <aroy34535@gmail.com>
2022-09-01 10:18:06 +0530

    my name and roll no

---
+++ name.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int roll[5];
+    char name[20];
+    printf("my name is:");
+    scanf("%9c",name);
+    printf("my roll no is:");
+    scanf("%d",&roll);
+    return 0;
+}
