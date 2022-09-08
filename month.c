8960d01e897a1d6fdc42f511961f89d606cc8ba1
aditiroy11 <aroy34535@gmail.com>
2022-09-08 22:03:46 +0530

    12 month in a year

---
+++ month.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int month;
+    printf("input month number to get the number:");
+    scanf("%d",&month);
+    if(month==1)
+    printf("january\n");
+   if(month==2)
+    printf("feb\n"); 
+    if(month==3)
+    printf("march\n");
+    if(month==4)
+    printf("april\n");
+    if(month==5)
+    printf("may\n");
+    if(month==6)
+    printf("jun\n");
+    if(month==7)
+    printf("july\n");
+    if(month==8)
+    printf("aug\n");
+    if(month==9)
+    printf("sep\n");
+    if(month==10)
+    printf("oct\n");
+    if(month==11)
+    printf("nov\n");
+    if(month==12)
+    printf("dec\n");
+    printf("Invaild month number!!\n");
+    return 0;
+}
