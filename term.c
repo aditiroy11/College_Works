6bc049a1ba3f9f7bb89cf83467ed357e85092050
aditiroy11 <aroy34535@gmail.com>
2022-09-15 21:04:41 +0530

    terms count

---
+++ term.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int term,count,n;//2^0
+    printf("how many terms:");
+    scanf("%d",&n);
+    count=0;
+    while (count<n){
+        term=pow(2,count);
+        printf("%3d,",term);
+        count++;
+    }
+    printf("\n");
+    return 0;
+    
+}
