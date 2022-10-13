01d5f88b76de52f66dd82fcda6e17a24f4f9249a
aditiroy11 <aroy34535@gmail.com>
2022-10-13 18:47:10 +0530

    array use to input sum

---
+++ inputsum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],sum=0,i,n;
+    printf("how many number:");
+    scanf("%d",&n);
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("sum = %d\n",sum);
+    printf("Avarage=%.2f",(float)sum/n);
+    
+}
